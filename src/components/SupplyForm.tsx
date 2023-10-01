b9473382910f67b4320af657199602b1b033e80c
Luis Jiménez <luisjimz94@gmail.com>
2023-09-30 22:27:54 -0600

    fix for http call

--- src/components/SupplyForm.tsx
+++ src/components/SupplyForm.tsx
@@ -2,14 +2,20 @@ import type { Supply } from "@/pages/api/supplies";
 import { ReactElement, useState } from "react";
 
 export function SupplyForm()  : ReactElement {
-  const [formData, setFormData] = useState({name: "",cost: "",type: ""});
-  function putSupply(form:any){
+  const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
+
+  function putSupply(e){
+    e.preventDefault();
     const requestOptions = {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: form
+      body: JSON.stringify( {
+        name,
+        cost
+      })
       };
-
+      
     fetch( '/api/supplies', requestOptions)
       .then((res) => res.json())
       .then((res) => {
@@ -20,14 +26,14 @@ export function SupplyForm()  : ReactElement {
   return(
     <div>
       <div className="text-center text-2xl">Crear nuevo insumo</div>
-      <form className="flex flex-col gap-3">
+      <form className="flex flex-col gap-3 ml-8">
         <label className="text-lg">
-          Nombre:<input className="border rounded" type="text" onChange={(name) => setName(name.)}/>
+          Nombre:<input className="border rounded" type="text" onChange={(name) => setName(name.target.value)}/>
         </label>
         <label>
           Cost:<input className="border rounded" type="number"/>
         </label>
-        <button className=" border rounded w-[200px]" type="submit" onSubmit={}>Submit</button>
+        <button className=" border rounded w-[200px]" type="submit" onClick={putSupply}>Submit</button>
       </form>
     </div>
   );
